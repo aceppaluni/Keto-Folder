ff57bdd196df8b449e9c7e213b6e80c75cb99a7f
Angelina Ceppaluni <aceppaluni@gmail.com>
2024-01-11 13:03:42 -0500

    postman

--- react/keto/components/recipesSlice.js
+++ react/keto/components/recipesSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 
-const url = 'http://localhost:3001'
+const url = 'http://localhost:5000'
 //const url = 'mongodb://localhost:27017/recipesdb'
 
 export const fetchRecipes = createAsyncThunk( 
